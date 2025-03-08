7be442caeddd2ad8adae38a79ce55fb9b9c2c108
raunit-dev <raunitjaiswal510@gmail.com>
2025-03-09 00:39:14 +0530

    fu

--- backend/routes/nft.routes.js
+++ backend/routes/nft.routes.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { mintPortfolio, mintBadge, getNFTs, verifyNFT } from '../controllers/nft.controller.js';
+import { mintPortfolio,getNFTs, verifyNFT } from '../controllers/nft.controller.js';
 
 const router = express.Router();
 
 router.post('/mintPortfolio', mintPortfolio);
-router.post('/mintBadge', mintBadge);
 router.get('/getNFTs/:wallet', getNFTs);
 router.get('/verify/:type/:id/:address', verifyNFT);
 
